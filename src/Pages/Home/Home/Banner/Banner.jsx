e8924a42d125d7676bc893878b0ff63c9d461280
JAHID HASAN <developerjahid@gmail.com>
2024-03-22 19:03:05 +0600

    banner intial done

---
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[40rem]">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src="https://media.licdn.com/dms/image/D4D12AQFkUKXahhNtVA/article-cover_image-shrink_720_1280/0/1693407093927?e=2147483647&v=beta&t=DYNceLcx84EGva2UJr6clVdlz_D6_DtaiVcGz1KFy-Y" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src="https://pencilbox.edu.bd/storage/467/700x400-Workshop-on-Graphics-Design.jpg" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
